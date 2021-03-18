713a0ac1889ee78ad6c45b4d2e54168905c65007
Pascal Heus <pascal.heus@gmail.com>
2021-03-18 14:27:39 -0600

    Renamed registry to _registry

--- utils/report.py
+++ utils/report.py
@@ -8,7 +8,53 @@ import jsonschema
 import logging
 import os
 from registry import *
+import requests
 import yaml
+
+
+def ckan(catalog, agency):
+    services = get_agency_services(catalog, agency)
+    catalogs = services.get('catalogs')
+    if catalogs:
+        for entry in catalogs:
+            if entry.get('platform') == 'ckan':
+                catalog_url = entry.get('endpoint')
+                api_endpoint = entry.get('api_endpoint')
+                if api_endpoint:
+                    api_url = api_endpoint
+                else:
+                    api_url = f"{entry.get('endpoint')}/api"
+                # get package count (using v1)
+                package_count = None
+                url = f"{api_url}/search/package?limit=0"
+                result = requests.get(url)
+                if result.status_code == 200:
+                    json = result.json()
+                    package_count = json.get('count')
+                # get resources count 9using v1)
+                resource_count = None
+                url = f"{api_url}/search/resource?limit=0"
+                result = requests.get(url)
+                if result.status_code == 200:
+                    json = result.json()
+                    package_count = json.get('count')
+                # DCAT
+                resource_count = None
+                url = f"{catalog_url}/catalog.jsonld"
+                result = requests.get(url)
+                if result.status_code == 200:
+                    json = result.json()
+                    is_dcat_catalog_found = True
+                else:
+                    is_dcat_catalog_found = False
+
+                # report
+                print("-"*20)
+                print(f"{catalog}/{agency} | {entry.get('name')}")
+                print(f"{catalog}/{agency} | {catalog_url}")
+                print(f"{catalog}/{agency} | {api_url}")
+                print(f"{catalog}/{agency} | packages: {package_count} | resources: {resource_count} | DCAT Catalog: {is_dcat_catalog_found}")
+    return
 
 def report_default(catalog, agency):
     report_ids(catalog, agency)
@@ -26,12 +72,13 @@ def report_ids(catalog, agency):
 def report_services(catalog, agency, platform=None, protocol=None):
     data = get_agency_services(catalog, agency)
     service_stats = {}
-    for key, value in data.items():
-        service_key = f"{value.get('type')}/{value.get('protocol')}"
-        if service_key in service_stats:
-            service_stats[service_key] += 1
-        else:
-            service_stats[service_key] = 1
+    for (type, services) in data.items():
+        for service in services:
+            service_key = f"{type}/{service.get('client')}"
+            if service_key in service_stats:
+                service_stats[service_key] += 1
+            else:
+                service_stats[service_key] = 1
     services = []
     for service_key, service_count in service_stats.items():
         services.append(f"{service_key}[{service_count}]")
@@ -63,10 +110,13 @@ def main():
         for agency in sorted(os.listdir(catalog_dir)):
             if args.agencies and agency not in args.agencies:
                 continue
-            if not args.reports:
+
+            if "default" in args.reports:
                 print("-"*20)
                 print(agency)
                 report_default(catalog,agency)
+            if "ckan" in args.reports:
+                ckan(catalog, agency)
             if "validate" in args.reports:
                 validate(catalog, agency, 'ids')
                 validate(catalog, agency, 'geo')
@@ -80,7 +130,7 @@ if __name__ ==  "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-a","--agencies", nargs="*", help="Agencies to include")
     parser.add_argument("-c","--catalogs", nargs="*", help="Catalogs to include")
-    parser.add_argument("-r","--reports", nargs="*", help="Reports to run")
+    parser.add_argument("-r","--reports", nargs="*", help="Reports to run: default | ckan | validation)", default='default')
     parser.add_argument("-ll","--loglevel", help="Python logging level", default="INFO")
     args = parser.parse_args()
 
